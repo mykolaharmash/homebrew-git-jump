88b8182d09cd7dafcc2caacaa9f3b751dff0bc33
Mykola Harmash <mykola.harmash@gmail.com>
2020-09-10 20:55:39 +0200

    v0.1.11

--- git-jump.rb
+++ git-jump.rb
@@ -3,8 +3,8 @@ require "language/node"
 class GitJump < Formula
   desc "Git branches helper: interactive UI and fuzzy-serach for Git branches"
   homepage "https://github.com/mykolaharmash/git-jump"
-  url "https://registry.npmjs.org/git-jump/-/git-jump-0.1.10.tgz"
-  sha256 "e80d72e75977b714d3419fbffc5b895088eb12061a19df9ec3ada2e4b63070e3"
+  url "https://registry.npmjs.org/git-jump/-/git-jump-0.1.11.tgz"
+  sha256 "a194a4caf408de3fafdbb76d6fcb69954e3118842aed275045a430618f3b0962"
   license "MIT"
 
   depends_on "node"
