aef46a194806b879fa9264906a0332463c204b7a
Mykola Harmash <mykola.harmash@gmail.com>
2020-09-10 19:10:18 +0200

    Fix shasum

--- git-jump.rb
+++ git-jump.rb
@@ -4,7 +4,7 @@ class GitJump < Formula
   desc "Git branches helper: interactive UI and fuzzy-serach for Git branches"
   homepage "https://github.com/mykolaharmash/git-jump"
   url "https://registry.npmjs.org/git-jump/-/git-jump-0.1.10.tgz"
-  sha256 "7e1bb39c4b885c74bc03d39cc75a39b8a3c5d196383455fa5e0fa7ca61509089"
+  sha256 "e80d72e75977b714d3419fbffc5b895088eb12061a19df9ec3ada2e4b63070e3"
   license "MIT"
 
   depends_on "node"
